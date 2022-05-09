4030bd0ad6ea4182ccb5df9207d5160988edf1df
Zakhar E. Likay <zlikay@kr.cs.gkovd.ru>
2022-05-09 23:22:41 +0700

    Made first DB upgrade

--- Test_DB_Migrate/app.py
+++ Test_DB_Migrate/app.py
@@ -14,6 +14,7 @@ class dev_types(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     creation_date = db.Column(db.DateTime, default=datetime)
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
