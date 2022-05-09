17763533aae79b0a4a0cff379fc290f025bfea6d
Zakhar E. Likay <zlikay@kr.cs.gkovd.ru>
2022-05-10 00:32:26 +0700

    Add page add_container

--- Test_DB_Migrate/app.py
+++ Test_DB_Migrate/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from datetime import datetime
@@ -6,15 +6,60 @@ from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///crjurnal.db'
-app.config['SQLALCEMY_TRACK_MODIFICATION'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
 
 class dev_types(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    creation_date = db.Column(db.DateTime, default=datetime)
-    
+    creation_date = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+class container_types(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    creation_date = db.Column(db.DateTime, default=datetime.utcnow)
+
+class connector_types(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    num_of_pins = db.Column(db.Integer, nullable=False)
+    is_male = db.Column(db.Boolean, nullable=False)
+    creation_date = db.Column(db.DateTime, default=datetime.utcnow)
+
+class containers(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    cont_type_id = db.Column(db.Integer, db.ForeignKey('connector_types.id'), nullable=False)
+    container_id = db.Column(db.Integer, db.ForeignKey('containers.id'), nullable=True)
+    creation_date = db.Column(db.DateTime, default=datetime.utcnow)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/add-container-type', methods=['POST', 'GET'])
+def add_container_type():
+    cont_types = container_types.query.order_by(container_types.title).all()
+    if request.method == 'POST':
+        title = request.form['title']
+        if title in cont_types:
+            return 'Такой контейнер уже есть!'
+        else:
+            cont_type = container_types(title=title)
+        try:
+            db.session.add(cont_type)
+            db.session.commit()
+            return redirect('/add-container-type')
+        except Exception as e:
+            return "Ошибка записи в БД: " + str(e)
+
+    return render_template('add-container-type.html', cont_types=cont_types)
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
