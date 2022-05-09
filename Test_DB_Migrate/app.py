098fe6d0c60fa172e37eeaaefdc643ff07970c23
Zakhar E. Likay <zlikay@kr.cs.gkovd.ru>
2022-05-09 23:12:54 +0700

    Migrations Inint

--- Test_DB_Migrate/app.py
+++ Test_DB_Migrate/app.py
@@ -1,13 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from datetime import datetime
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///crjurnal.db'
 app.config['SQLALCEMY_TRACK_MODIFICATION'] = False
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-class dev_types(db.Model)
+class dev_types(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     creation_date = db.Column(db.DateTime, default=datetime)
