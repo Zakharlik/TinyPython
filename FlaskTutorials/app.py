3aad92f06ad16d1d1c94292b1c55d387bade1200
Zakhar E. Likay <zakhar.likay@mail.ru>
2022-05-09 15:06:40 +0700

    Init DB in app

--- FlaskTutorials/app.py
+++ FlaskTutorials/app.py
@@ -1,6 +1,22 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+## app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False ## FSADeprecationWarning: SQLALCHEMY_TRACK_MODIFICATIONS adds significant overhead and will be disabled by default in the future.  Set it to True or False to suppress this warning.
+db = SQLAlchemy(app)
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    intro = db.Column(db.String(300), nullable=False)
+    test = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Article %r>' % self.id
+
 
 @app.route('/')
 @app.route('/home')
